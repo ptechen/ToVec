1d2229ebbc33f24cb1721b53453c886aaf226cf3
taochen <taochen@yunjumai.com>
2022-05-25 21:28:43 +0800

    update

--- struct2vec/src/lib.rs
+++ struct2vec/src/lib.rs
@@ -1,8 +1,13 @@
 use serde_json::Value;
 use std::collections::HashMap;
-// 
-// #[cfg(feature = "struct2vec_derive")]
-// pub use struct2vec_derive::ToVec;
+
+#[cfg(feature = "struct2vec_derive")]
+#[allow(unused_imports)]
+#[macro_use]
+extern crate struct2vec_derive;
+#[cfg(feature = "struct2vec_derive")]
+#[doc(hidden)]
+pub use struct2vec_derive::*;
 
 pub trait ToVec {
     fn to_vec(&self) -> Vec<HashMap<String, Value>>;
